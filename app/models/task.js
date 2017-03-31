08e332cb44664f0ed10376568ba7a0a04e7076f1
Lidia <lidia.f.garces@gmail.com>
2017-03-31 18:06:18 +0200

    New version accepting number of answers

--- app/models/task.js
+++ app/models/task.js
@@ -5,6 +5,9 @@ module.exports = mongoose.model('Task', new Schema({
     title: String,
     description: String,
     reward: Number,
+    number_of_answers: Number,
+    flag_out_users: [String],
+    review_task: Boolean,
     location: {
     	coordinate_x: String,
     	coordinate_y: String,
@@ -20,5 +23,17 @@ module.exports = mongoose.model('Task', new Schema({
     		},*/
     	item_options: [String],
     	item_answer_type: String,
-    }]
+    }],
+    answers: [{
+	    user_id: String,
+	    questions_answers: [{
+	    	answer_text: String,
+	        answer_picture_url: String
+	    	/*answer_picture: 
+	    		{ 
+	    			data: Buffer, 
+	    			contentType: String 
+	    		}*/
+	    }]
+	}]
 }));
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -10,15 +10,17 @@ router.route('/tasks')
 .post(function(req,res){
 
     var task = new Task();
-
-    console.log(req.body);
   
     task.title = req.body.title; 
     task.description = req.body.description; 
     task.reward = req.body.reward;
+    task.number_of_answers = req.body.number_of_answers;
+    task.flag_out_users = req.body.flag_out_users;
+    task.review_task = req.body.review_task;
     task.location = req.body.location;
     task.items = req.body.items;
     task.answer_type = req.body.answer_type;
+    task.answers = [];
 
     var mandatoryFilled = task.title && task.description && task.reward && (task.items.length>0);
 
@@ -27,18 +29,17 @@ router.route('/tasks')
         if (!req.body.items[itemIdx].item_answer_type) mandatoryFilled=false;
     }
 
-    //task.friends.push(req.body.friends);
     if(req.body.items.length>10) {
         res.status('400').send('Bad Request. The maximum number of item is 10.');
     }else{
         if(mandatoryFilled){
             task.save(function(err, task){
+
                 if (err) { res.send(err); }
                 res.json({ message: 'We have created a new task! The id for the task is ' + task.id, task_id: task.id});
             });
         }else{
             res.status('400').send('Bad Request. A mandatory field is empty.');
-            console.log(task.title + '\n' + task.description + '\n' + task.reward + '\n' + task.items + '\n' + task.items.item_question + '\n' + task.items.item_answer_type);
         }
     }
 
@@ -46,8 +47,20 @@ router.route('/tasks')
 
 .get(function(req,res){
 
-    Task.find(function(err, tasks){
-        if (err){ res.send(err); }
+    Task.find().select('-answers').exec(function(err, tasks){
+        if (err){ return res.send(err); }
+
+        res.json(tasks);
+    });
+
+})
+
+router.route('/tasks/todo')
+
+.get(function(req,res){
+
+    Task.find({ $where : "this.answers.length >= this.number_of_answers" }).select('-answers').exec(function(err, tasks){
+        if (err){ return res.send(err); }
 
         res.json(tasks);
     });
@@ -59,7 +72,7 @@ router.route('/tasks/:task_id')
 .get(function (req,res){
 
     Task.findById(req.params.task_id, function(err, task){
-        if (err){ res.send(err); }
+        if (err){ return res.send(err); }
 
         res.json(task);
     });
@@ -67,14 +80,18 @@ router.route('/tasks/:task_id')
 
 .put(function(req,res){
     Task.findById(req.params.task_id, function(err, task){
-        if (err){ res.send(err); }
+        if (err){ return res.send(err); }
 
         task.title = req.body.title; 
         task.description = req.body.description; 
         task.reward = req.body.reward;
+        task.number_of_answers = req.body.number_of_answers;
+        task.flag_out_users = req.body.flag_out_users;
+        task.review_task = req.body.review_task;
         task.location = req.body.location;
         task.items = req.body.items;
         task.answer_type = req.body.answer_type;
+        task.answers = [];
 
         task.save(function(err){
             if (err) { res.send(err); }
@@ -86,21 +103,20 @@ router.route('/tasks/:task_id')
 
 .delete(function(req, res){
     Task.remove({_id:req.params.task_id}, function(err, task){
-        if (err){ res.send(err); }
+        if (err){ return res.send(err); }
 
         res.json({ message: 'Task successfully removed!' });
     });
 })
 
-// Routes for a specific task, ending in /tasks/:task_id
 router.route('/tasks/:task_id/answers')
 
 .get(function (req,res){
 
-    Answer.find({task_id: req.params.task_id}, function(err, answers){
-        if (err){ res.send(err); }
+    Task.find({task_id: req.params.task_id}, function(err, answers){
+        if (err){ return res.send(err); }
 
-        res.json(answers);
+        res.json(task.answers);
     });
 })
 
@@ -108,25 +124,24 @@ router.route('/answers')
 
 .post(function(req,res){
 
-    var answer = new Answer();
-
     console.log(req.body);
-  
-    // Set the task name, and add our friend to the friends array 
-    answer.user_id = req.body.user_id; 
-    answer.task_id = req.body.task_id; 
-    answer.answers = req.body.answers; 
 
-    //task = req.body;
+    if(req.body.task_id && (req.body.answers.length>0)){
+        Task.findById(req.body.task_id, function(err, task){
+            if (err){ return res.send(err); }
+            console.log(task);
 
-    //task.friends.push(req.body.friends); 
+            var answer = {};
 
-    if(answer.task_id && (answer.answers.length>0)){
-        answer.save(function(err, answer){
-            if (err) { res.send(err); }
-            console.log('\n');
-            console.log(answer.id);
-            res.json({ message: 'We have created a new answer! The id of the answer is ' + answer.id, answer_id: answer.id});
+            answer.user_id = req.body.user_id; 
+            answer.questions_answers = req.body.answers;
+
+            task.answers.push(answer);
+
+            task.save(function(err, updatedTask){
+                if (err) { res.send(err); }
+                res.json({ message: 'We have created a new answer! The id of the answer is ' + task.answers[task.answers.length-1].id, answer_id: task.answers[task.answers.length-1].id});
+            });
         });
     }else{
         res.status('400').send('Bad Request. A mandatory field is empty.');
@@ -135,47 +150,54 @@ router.route('/answers')
 
 .get(function(req,res){
 
-    Answer.find(function(err, answers){
-        if (err){ res.send(err); }
+    Task.find(function(err, tasks){
+        if (err){ return res.send(err); }
 
-        res.json(answers);
+        res.json(tasks);
     });
 
 })
 
-router.route('/answers/:answer_id')
+router.route('/tasks/:task_id/answers/:answer_id')
 
 .get(function (req,res){
 
-    Answer.findById(req.params.answer_id, function(err, answer){
-        if (err){ res.send(err); }
+    Task.findById(req.params.task_id, function(err, task){
+        if (err){ return res.send(err); }
+        if(!task.answers.id(req.params.answer_id)) { return res.json({ message: 'There is not answer with id ' + req.params.answer_id + ' in task ' + req.params.task_id }) };;
 
-        res.json(answer);
+        res.json(task.answers.id(req.params.answer_id));
     });
 })
 
 .put(function(req,res){
-    Answer.findById(req.params.answer_id, function(err, answer){
-        if (err){ res.send(err); }
+    Task.findById(req.params.task_id, function(err, task){
+        if (err){ return res.send(err); }
+        if(!task.answers.id(req.params.answer_id)) { return res.json({ message: 'There is not answer with id ' + req.params.answer_id + ' in task ' + req.params.task_id }) };;
+        task.answers.id(req.params.answer_id).user_id=req.body.user_id;
+        task.answers.id(req.params.answer_id).questions_answers=req.body.answers;
 
-        answer.user_id = req.body.user_id; 
-        answer.task_id = req.body.task_id; 
-        answer.answers = req.body.answers; 
-
-        answer.save(function(err){
+        task.save(function(err){
             if (err) { res.send(err); }
 
-            res.json({ message: 'Answer Updated!' });
+            res.json({ message: 'Answer updated!' });
         });
     });
 })
 
 .delete(function(req, res){
-    Answer.remove({_id:req.params.answer_id}, function(err, answer){
-        if (err){ res.send(err); }
+    Task.findById(req.params.task_id, function(err, task){
+        if (err){ return res.send(err); }
+        if(!task.answers.id(req.params.answer_id)) { return res.json({ message: 'There is not answer with id ' + req.params.answer_id + ' in task ' + req.params.task_id }) };;
 
-        res.json({ message: 'Successfully removed!' });
+        task.answers.pull({'_id': req.params.answer_id});
+
+        task.save(function(err, updatedTask){
+            if (err) { res.send(err); }
+            res.json({ message: 'We have deleted the answer!'});
+        });
     });
+
 })
 
 module.exports = router;
